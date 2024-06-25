4eba584bac3613053f2ca4f6d543ebcae20074b5
kleber silva <klebersilva805@gmail.com>
2024-06-25 15:12:30 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({ reset: true})
+
+sr.reveal(".Texto-Superior", {duration: 1000});
+
+sr.reveal(".Numeros-Informativos", {duration: 1000});
+
+sr.reveal(".TextoIntermediario", {duration: 1000});
+
+sr.reveal(".Imagem",{ 
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 4000});
+
+sr.reveal(".Cadastro", {duration: 1000});
+
+sr.reveal("input", {duration: 1000});
+
+sr.reveal("button", {duration: 1000});
